2b8734dc907c8b9ba887e9a2fa7a20ad25793a50
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2023-01-27 20:15:44 -0300

    Alteração nos details

--- src/pages/DrinksDetails.jsx
+++ src/pages/DrinksDetails.jsx
@@ -1,15 +1,16 @@
 import React, { useContext, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import MainContext from '../context/MainContext';
-import RecipeDetails from './RecipeDetails';
+import RecipeDetails from '../components/RecipeDetails';
 
 function DrinksDetails() {
   const { id } = useParams();
-  const { detailsFetch } = useContext(MainContext);
+  const { fetchApi, detailsFetch } = useContext(MainContext);
 
   useEffect(() => {
     const fetchDetails = async () => {
       await detailsFetch.fetchApi(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
+      await fetchApi('https://www.themealdb.com/api/json/v1/1/search.php?s=');
     };
     fetchDetails();
   }, []);
