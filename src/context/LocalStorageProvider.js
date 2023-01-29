20c5a600142213181fca900a803aa08bfd1879a7
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2023-01-29 13:42:31 -0300

    Implementação da página Done Recipes, sem os testes

--- src/context/LocalStorageProvider.js
+++ src/context/LocalStorageProvider.js
@@ -3,8 +3,30 @@ import { useMemo } from 'react';
 import LocalStorageContext from './LocalStorageContext';
 import useLocalStorage from '../hook/useLocalStorage';
 
+const teste = [{
+  id: '52977',
+  type: 'meal',
+  nationality: 'Germany',
+  category: 'side',
+  alcoholicOrNot: '',
+  name: 'Corba',
+  image: 'https://www.themealdb.com/images/media/meals/58oia61564916529.jpg',
+  doneDate: '28/02/2022',
+  tags: ['food', 'exotic', 'best'],
+}, {
+  id: '15997',
+  type: 'drink',
+  nationality: '',
+  category: '',
+  alcoholicOrNot: 'Optional alcohol',
+  name: 'GG',
+  image: 'https://www.thecocktaildb.com/images/media/drink/vyxwut1468875960.jpg',
+  doneDate: '28/01/2023',
+  tags: ['ala'],
+}];
+
 function LocalStorageProvider({ children }) {
-  const [doneRecipes, setDoneRecipes] = useLocalStorage('doneRecipes', []);
+  const [doneRecipes, setDoneRecipes] = useLocalStorage('doneRecipes', teste);
   const [favoriteRecipes, setFavoriteRecipes] = useLocalStorage('favoriteRecipes', []);
   const [inProgressRecipes,
     setInProgressRecipes,
