a25acd3542f536387d2d055cfcbc17d5f6454f42
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2023-01-29 16:16:50 -0300

    Adiciona página de favoritos

---
+++ src/components/ListFavorites.jsx
@@ -0,0 +1,99 @@
+import React, { useContext, useState } from 'react';
+import { Link } from 'react-router-dom';
+import LocalStorageContext from '../context/LocalStorageContext';
+import MainContext from '../context/MainContext';
+import shareIcon from '../images/shareIcon.svg';
+import blackHeart from '../images/blackHeartIcon.svg';
+
+const copy = require('clipboard-copy');
+
+function ListFavorites() {
+  const { values, functions } = useContext(LocalStorageContext);
+  const { doneFilter } = useContext(MainContext);
+  const { favoriteRecipes } = values;
+  const { removeFavorite } = functions;
+
+  console.log(values);
+
+  const [isShared, setIsShared] = useState(new Array(favoriteRecipes.length).fill(false));
+
+  function handleClickShare(type, id, position) {
+    if (type === 'meal') {
+      copy(`http://localhost:3000/meals/${id}`);
+    } else {
+      copy(`http://localhost:3000/drinks/${id}`);
+    }
+
+    const updatedCheckedState = isShared
+      .map((item, index) => (index === position ? !item : item));
+
+    setIsShared(updatedCheckedState);
+  }
+
+  return (
+    <div>
+      {
+        favoriteRecipes.length > 0 && (
+          favoriteRecipes
+            .filter((item) => item.type.includes(doneFilter)).map((item, index) => (
+              <div key={ `${item.name}${index}` }>
+                <Link
+                  to={
+                    item.type === 'meal' ? `/meals/${item.id}` : `/drinks/${item.id}`
+                  }
+                >
+                  <img
+                    data-testid={ `${index}-horizontal-image` }
+                    src={ item.image }
+                    alt={ item.name }
+                    width="200px"
+                  />
+                  <h3 data-testid={ `${index}-horizontal-name` }>{item.name}</h3>
+                </Link>
+                {
+                  item.type === 'meal' ? (
+                    <h3
+                      data-testid={ `${index}-horizontal-top-text` }
+                    >
+                      {`${item.nationality} - ${item.category}`}
+
+                    </h3>)
+                    : (
+                      <h3
+                        data-testid={ `${index}-horizontal-top-text` }
+                      >
+                        {item.alcoholicOrNot}
+                      </h3>)
+                }
+                <button
+                  type="button"
+                  onClick={ () => removeFavorite(item.id) }
+                >
+                  <img
+                    data-testid={ `${index}-horizontal-favorite-btn` }
+                    src={ blackHeart }
+                    alt="favorite"
+                  />
+                </button>
+                <button
+                  type="button"
+                  onClick={ () => handleClickShare(item.type, item.id, index) }
+                >
+                  <img
+                    data-testid={ `${index}-horizontal-share-btn` }
+                    src={ shareIcon }
+                    alt="share"
+                  />
+                </button>
+                {
+                  isShared[index] && <small>Link copied!</small>
+                }
+              </div>
+            ))
+        )
+      }
+    </div>
+  );
+}
+
+export default ListFavorites;
--- src/context/LocalStorageProvider.js
+++ src/context/LocalStorageProvider.js
@@ -3,9 +3,28 @@ import { useMemo } from 'react';
 import LocalStorageContext from './LocalStorageContext';
 import useLocalStorage from '../hook/useLocalStorage';
 
+const teste = [{
+  id: '52977',
+  type: 'meal',
+  nationality: 'Germany',
+  category: 'side',
+  alcoholicOrNot: '',
+  name: 'Corba',
+  image: 'https://www.themealdb.com/images/media/meals/58oia61564916529.jpg',
+}, {
+  id: '15997',
+  type: 'drink',
+  nationality: '',
+  category: '',
+  alcoholicOrNot: 'Optional alcohol',
+  name: 'GG',
+  image: 'https://www.thecocktaildb.com/images/media/drink/vyxwut1468875960.jpg',
+}];
+
 function LocalStorageProvider({ children }) {
   const [doneRecipes, setDoneRecipes] = useLocalStorage('doneRecipes', []);
-  const [favoriteRecipes, setFavoriteRecipes] = useLocalStorage('favoriteRecipes', []);
+  const [favoriteRecipes,
+    setFavoriteRecipes] = useLocalStorage('favoriteRecipes', teste);
   const [inProgressRecipes,
     setInProgressRecipes,
   ] = useLocalStorage('inProgressRecipes', {});
