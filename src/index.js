1a05302889c395c909ee522e05185abf67623302
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2023-02-01 14:38:35 -0300

    Add - Style Login

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './style/reset.css';
 
 ReactDOM
   .createRoot(document.getElementById('root'))
