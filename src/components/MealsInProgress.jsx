c788899aa601d2507042b20676b75021e6616bc2
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2023-02-03 13:29:00 -0300

    Add - Style InProgress

--- src/components/MealsInProgress.jsx
+++ src/components/MealsInProgress.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-max-depth */
 import React, { useContext, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import shareIcon from '../images/shareIcon.svg';
@@ -6,6 +7,18 @@ import LocalStorageContext from '../context/LocalStorageContext';
 import notFavorited from '../images/whiteHeartIcon.svg';
 import favorited from '../images/blackHeartIcon.svg';
 import '../style/RecipeInProgress.css';
+import '../style/MealsInProgress.css';
+import beef from '../images/beef.png';
+import breakfast from '../images/breakfast.png';
+import chicken from '../images/chicken.png';
+import dessert from '../images/dessert.png';
+import goat from '../images/goat.png';
+import ordinary from '../images/ordinary.png';
+import cocktail from '../images/cocktail.png';
+import shake from '../images/shake.png';
+import other from '../images/other.png';
+import cocoa from '../images/cocoa.png';
+import foodDrink from '../images/food-drink.png';
 
 const copy = require('clipboard-copy');
 
@@ -15,6 +28,33 @@ function MealsInProgress() {
   const [isShared, setIsShared] = useState(false);
 
   const history = useHistory();
+
+  function handleCategory(category) {
+    switch (category) {
+    case 'Beef':
+      return beef;
+    case 'Breakfast':
+      return breakfast;
+    case 'Chicken':
+      return chicken;
+    case 'Dessert':
+      return dessert;
+    case 'Goat':
+      return goat;
+    case 'Ordinary Drink':
+      return ordinary;
+    case 'Cocktail':
+      return cocktail;
+    case 'Shake':
+      return shake;
+    case 'Other / Unknown':
+      return other;
+    case 'Cocoa':
+      return cocoa;
+    default:
+      return foodDrink;
+    }
+  }
 
   const whiteHeart = <img src={ notFavorited } data-testid="favorite-btn" alt="addFav" />;
   const blackHeart = <img src={ favorited } data-testid="favorite-btn" alt="rmvFav" />;
@@ -73,72 +113,98 @@ function MealsInProgress() {
     <div>
       {
         detailsFetch.dataValue.meals && detailsFetch.dataValue.meals.map((item) => (
-          <div key={ item.strMeal }>
-            <button
-              data-testid="share-btn"
-              onClick={ handleClickShare }
-            >
-              <img src={ shareIcon } alt="share" />
-            </button>
-            <button
-              onClick={ () => functions.handleFavorite({
-                id: item.idMeal,
-                type: 'meal',
-                nationality: item.strArea,
-                category: item.strCategory,
-                alcoholicOrNot: '',
-                name: item.strMeal,
-                image: item.strMealThumb,
-              }) }
-            >
-              {functions.isFavoriteRecipe(item.idMeal) ? blackHeart : whiteHeart}
-            </button>
-            {
-              isShared && <small>Link copied!</small>
-            }
-            <img
-              data-testid="recipe-photo"
-              src={ item.strMealThumb }
-              alt={ item.strMeal }
-            />
-            <h2 data-testid="recipe-title">{ item.strMeal }</h2>
-            <h4 data-testid="recipe-category">{ item.strCategory }</h4>
-            {
-              ingredients.map((element, index) => {
-                const { idMeal } = item;
-                const checked = functions.isAddedIngredient('meals', idMeal, element);
-                return (
-                  <label
-                    className={ checked ? 'line-through' : '' }
-                    htmlFor={ `${element}${index}` }
-                    data-testid={ `${index}-ingredient-step` }
-                    key={ `${element}${index}` }
+          <div className="inprogress-container" key={ item.strMeal }>
+            <div className="inprogress-header">
+              <img
+                className="inprogress-header-image"
+                data-testid="recipe-photo"
+                src={ item.strMealThumb }
+                alt={ item.strMeal }
+              />
+              <div className="inprogress-header-btn-container">
+                <div className="inprogress-btn-container">
+                  <button
+                    className="inprogress-header-btn-share"
+                    data-testid="share-btn"
+                    onClick={ handleClickShare }
                   >
-                    {
-                      ` ${element}`
-                    }
-                    <input
-                      type="checkbox"
-                      name={ `${element}${index}` }
-                      id={ `${element}${index}` }
-                      checked={ checked }
-                      onChange={ () => {
-                        functions.handleIngredient('meals', idMeal, element);
-                      } }
-                    />
-                  </label>
-                );
-              })
-            }
-            <p data-testid="instructions">{item.strInstructions}</p>
-            <iframe
-              data-testid="video"
-              title={ item.strMeal }
-              width="420"
-              height="315"
-              src={ item.strYoutube }
-            />
+                    <img src={ shareIcon } alt="share" />
+                  </button>
+                  <button
+                    className="inprogress-header-btn-fav"
+                    onClick={ () => functions.handleFavorite({
+                      id: item.idMeal,
+                      type: 'meal',
+                      nationality: item.strArea,
+                      category: item.strCategory,
+                      alcoholicOrNot: '',
+                      name: item.strMeal,
+                      image: item.strMealThumb,
+                    }) }
+                  >
+                    {functions.isFavoriteRecipe(item.idMeal) ? blackHeart : whiteHeart}
+                  </button>
+                </div>
+                {
+                  isShared && (
+                    <small
+                      className="inprogress-header-shared"
+                    >
+                      Link copied!
+                    </small>)
+                }
+
+              </div>
+              <div className="inprogress-category-container">
+                <img src={ handleCategory(item.strCategory) } alt="category" />
+                <h4 data-testid="recipe-category">{ item.strCategory }</h4>
+              </div>
+              <div className="inprogress-title-container">
+                <h2 data-testid="recipe-title">{ item.strMeal }</h2>
+              </div>
+            </div>
+            <div className="inprogress-ingredients-container">
+              <h3 className="inprogress-ingredients-title">Ingredients</h3>
+              {
+                ingredients.map((element, index) => {
+                  const { idMeal } = item;
+                  const checked = functions.isAddedIngredient('meals', idMeal, element);
+                  return (
+                    <div
+                      className="inprogress-checkbox-container"
+                      key={ `${element}${index}` }
+                    >
+                      <input
+                        className="form-check-input inprogress-checkbox"
+                        type="checkbox"
+                        name={ `${element}${index}` }
+                        id={ `${element}${index}` }
+                        checked={ checked }
+                        onChange={ () => {
+                          functions.handleIngredient('meals', idMeal, element);
+                        } }
+                      />
+                      <label
+                        className={ checked ? 'line-through' : '' }
+                        htmlFor={ `${element}${index}` }
+                        data-testid={ `${index}-ingredient-step` }
+                      >
+                        {
+                          ` ${element}`
+                        }
+                      </label>
+
+                    </div>
+                  );
+                })
+              }
+            </div>
+            <div className="inprogress-instruction-container">
+              <h3 className="inprogress-ingredients-title">Instructions</h3>
+              <p data-testid="instructions">{item.strInstructions}</p>
+            </div>
             <button
+              className="inprogress-btn-finish"
               data-testid="finish-recipe-btn"
               disabled={ !functions.isDoneRecipe('meals', item.idMeal, ingredients) }
               onClick={ () => { handleClickFinish(item); } }
