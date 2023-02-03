51315982fdd6c337c6c09838e212fee845087dac
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2023-02-03 15:37:29 -0300

    Add - Style DoneRecipes

--- src/pages/DoneRecipes.jsx
+++ src/pages/DoneRecipes.jsx
@@ -1,10 +1,11 @@
 import Header from '../components/Header';
 import FilterDoneRecipes from '../components/FilterDoneRecipes';
 import RecipesDone from '../components/RecipesDone';
+import '../style/DoneRecipes.css';
 
 function DoneRecipes() {
   return (
-    <div>
+    <div className="donerecipes-container">
       <Header />
       <FilterDoneRecipes />
       <RecipesDone />
