0365a480b33fbf1d43fefb44d0b9bb5f132d2876
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2023-02-03 13:54:44 -0300

    Add - Style FilterDoneRecipes

--- src/components/FilterDoneRecipes.jsx
+++ src/components/FilterDoneRecipes.jsx
@@ -1,26 +1,36 @@
 import { useContext } from 'react';
 import MainContext from '../context/MainContext';
+import foodDrink from '../images/food-drink.png';
+import imgMeal from '../images/mealIcon.svg';
+import imgDrink from '../images/drinkIcon.svg';
+import '../style/FilterDoneRecipes.css';
 
 function FilterDoneRecipes() {
   const { setDoneFilter } = useContext(MainContext);
   return (
-    <div>
+    <div className="filterdone-container">
       <button
+        className="filterdone-btn"
         data-testid="filter-by-all-btn"
         onClick={ () => setDoneFilter('') }
       >
+        <img src={ foodDrink } alt="all" />
         All
       </button>
       <button
+        className="filterdone-btn"
         data-testid="filter-by-meal-btn"
         onClick={ () => setDoneFilter('meal') }
       >
+        <img src={ imgMeal } alt="meal" />
         Meals
       </button>
       <button
+        className="filterdone-btn"
         data-testid="filter-by-drink-btn"
         onClick={ () => setDoneFilter('drink') }
       >
+        <img src={ imgDrink } alt="drink" />
         Drinks
       </button>
     </div>
