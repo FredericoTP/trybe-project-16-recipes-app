b61fb6641be832aa6311d84408a93fc093270ae8
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2023-02-03 16:42:00 -0300

    Add - Style ProfileCard

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,4 +1,8 @@
 import { useHistory } from 'react-router-dom';
+import doneIcon from '../images/done.png';
+import favoriteIcon from '../images/favorite.png';
+import logoutIcon from '../images/logout.png';
+import '../style/ProfileCard.css';
 
 function ProfileCard() {
   const history = useHistory();
@@ -7,10 +11,11 @@ function ProfileCard() {
     history.push('/');
   };
   return (
-    <>
+    <div className="profilecard-container">
       {
         localStorage.getItem('user') && (
           <h3
+            className="profile-user"
             data-testid="profile-email"
           >
             { JSON.parse(localStorage.getItem('user')).email }
@@ -18,24 +23,30 @@ function ProfileCard() {
         )
       }
       <button
+        className="profile-btn"
         data-testid="profile-done-btn"
         onClick={ () => history.push('/done-recipes') }
       >
+        <img className="profile-icon-image" src={ doneIcon } alt="done" />
         Done Recipes
       </button>
       <button
+        className="profile-btn"
         data-testid="profile-favorite-btn"
         onClick={ () => history.push('/favorite-recipes') }
       >
+        <img className="profile-icon-image" src={ favoriteIcon } alt="favorite" />
         Favorite Recipes
       </button>
       <button
+        className="profile-btn"
         data-testid="profile-logout-btn"
         onClick={ handleLogoutButton }
       >
+        <img className="profile-icon-image" src={ logoutIcon } alt="logout" />
         Logout
       </button>
-    </>
+    </div>
   );
 }
 
